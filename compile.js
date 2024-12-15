709b4f9c3cae66d57b20c31e9b301dad90842520
hsoni92 <h.soni1321992@gmail.com>
2024-12-15 18:16:00 +0530

    himanshu.soni-15-12-2024-18:16:00

--- compile.js
+++ compile.js
@@ -3,7 +3,8 @@ const pdflatex = spawn('pdflatex', ['-output-directory', './', 'resume.tex']);
 
 // Capture stdout (standard output)
 pdflatex.stdout.on('data', (data) => {
-    console.log('stdout: ' + data.toString());
+    // enable if verbose is needed
+    // console.log('stdout: ' + data.toString());
 });
 
 // Capture stderr (standard error)
@@ -11,12 +12,16 @@ pdflatex.stderr.on('data', (data) => {
     console.error('stderr: ' + data.toString());
 });
 
-// Handle exit event
-pdflatex.on('exit', function (code) {
-    console.log('Child process exited with code ' + code);
-});
-
 // Handle errors during process spawning
 pdflatex.on('error', (err) => {
     console.error('Failed to start subprocess:', err);
+});
+
+// Handle exit event
+pdflatex.on('exit', function (code) {
+    if (code === 0) {
+        console.log(`successfully generated resume.pdf from resume.tex`);
+    } else {
+        console.log('Child process exited with code ' + code);
+    }
 });
